7701595fc70148f5ddef1e01be08bb2f7a007eab
Carson Smith <smittycart127@outlook.com>
2024-07-25 03:30:54 -0500

    create navbar, create pages, add music content, create landing page

---
+++ static-sources/src/components/GithubIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaGithub } from "react-icons/fa6";
+
+const GithubIcon = () => {
+    return (
+        <a href="https://github.com/cart0nn" target="_blank" rel="noopener noreferrer">
+            <FaGithub className="text-4xl text-white hover:text-gray-500 transition-colors duration-200"/>
+        </a>
+    )
+}
+
+export default GithubIcon;
---
+++ static-sources/src/components/LinkedinIcon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FaLinkedinIn } from "react-icons/fa";
+
+const LinkedinIcon = () => {
+    return (
+        <a href="https://www.linkedin.com/in/carson-smith-0a624822a/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedinIn className="text-4xl text-white hover:text-gray-500 transition-colors duration-200"/>
+        </a>
+    )
+}
+
+export default LinkedinIcon

75289015193781b5cfe3f04014aa3ff924bd7eb8
Carson Smith <smittycart127@outlook.com>
2024-07-25 03:43:17 -0500

    fix deployment

--- static-sources/astro.config.mjs
+++ static-sources/astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://cart0nn.github.io',
 });

6c648c334a8ce293031e5196a5286d7631def4ec
Carson Smith <smithcarson1290@gmail.com>
2024-09-09 17:32:07 -0500

    im fucking stupid lmao

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 
 content_path="$WEBSERVER_ROOT/dist"
 
-git pull
-
-npm run build
-
 if test -d content_path; then
   echo 'Webserver content already exists, removing.'
   rm -r content_path
@@ -15,6 +11,10 @@ if test -d dist; then
   echo 'Old static files present, removing.'
   rm -r dist
 fi
+
+git pull
+
+npm run build
 
 echo "Copying static files to server root."
 cp -r dist content_path
